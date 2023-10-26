99ca1a601baf8b505d854e91762febbd4cdbe972
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-10-26 03:03:39 -0700

    wrote reaction schema and field specs

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,43 @@
 const mongoose = require("mongoose");
 
+function formatDate(date) {
+    let options = { month: 'long', day: 'numeric', year: 'numeric' };
+    return `${date.toLocaleDateString(undefined, options)} at ${date.toLocaleTimeString()}`;
+    // example: October 25, 2023 at 8:47:48 PM
+    // note: my timezone is PDT. your results may vary if your timezone is different.
+
+    // source for toLocaleDateString(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+    // source for toLocaleTimeString(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
+}
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: formatDate
+    }
+},
+{
+    toJSON: {
+        getters: true // enable getters
+    },
+    id: false
+});
+// source for how to use getter functions in mongoose: https://mongoosejs.com/docs/tutorials/getters-setters.html
+
 const thoughtSchema = new mongoose.Schema({
     thoughtText: {
         type: String,
@@ -9,13 +47,25 @@ const thoughtSchema = new mongoose.Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: formatDate
     },
     username: {
         type: String,
         required: true
     },
-    reactions: []
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
 });
 
 const thoughtModel = mongoose.model('thought', thoughtSchema);
--- models/user.js
+++ models/user.js
@@ -11,7 +11,26 @@ const userSchema = new mongoose.Schema({
         type: String,
         unique: true,
         required: true,
-        // match: // regex to validate email
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address'] // regex to validate email
+        /*
+        the regex used in match can be broken down as follows:
+        /
+        1) .+
+        2) @
+        3) .+
+        4) \.
+        5) .+
+        /
+
+        1) .+ matches any character one or more times
+        2) @ matches the character @ literally
+        3) .+ matches any character one or more times
+        4) \. matches the character . literally
+        5) .+ matches any character one or more times
+
+        for example, orange@gmail.com would be a valid email address
+        source: https://regex101.com/
+        */
     },
     thoughts: [{
         type: mongoose.Schema.Types.ObjectId,
@@ -21,10 +40,18 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }],
-    // userSchema.virtual('friendCount').get(function() {
-    //     return this.friends.length;
-    // });
+},
+{
+    toJSON: {
+        virtuals: true, // enable virtuals
+    },
+    id: false
 });
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+// source for how to use virtuals in mongoose: https://mongoosejs.com/docs/tutorials/virtuals.html
 
 const userModel = mongoose.model('user', userSchema); // create a model from the user schema
 
